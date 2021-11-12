462da685d8b727337bf4c6e8a031b5378965e020
Steven <mlmjm123@gmail.com>
2021-11-12 17:21:18 -0600

    read array cards

---
+++ globalData.py
@@ -0,0 +1,16 @@
+def buildArrayCards(palo):
+  "build array from 1 - 13"
+  cards= []
+  for i in range(1, 13):
+    cardRoute= "./images/handCards/"
+    cards.append(cardRoute + str(i) + palo + '.png')
+  return cards
+
+treboles= buildArrayCards('t')
+corazones= buildArrayCards('c')
+diamantes= buildArrayCards('d')
+picas= buildArrayCards('p')
+
+allCards = treboles + corazones + diamantes + picas
+
+testCards= ["./images/handCards/carta1.png", "./images/handCards/carta2.png"]
