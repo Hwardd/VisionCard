b270ce209519dbcf365915f37e7176c13d03ab72
Hwardd <dxeuler101@hotmail.com>
2021-11-12 19:19:25 -0600

    color para el kros que riko

--- holo.py
+++ holo.py
@@ -20,6 +20,7 @@ while(1):
     img = ImageGrab.grab(bbox=(100,100,1200,900)) #bbox specifies specific region (bbox= x,y,width,height *starts top-left)
     img_np = np.array(img) #this is the array obtained from conversion
     img_gray = cv.cvtColor(img_np, cv.COLOR_BGR2GRAY)
+    img_show = cv.cvtColor(img_np,cv.COLOR_BGR2RGB)
     
     for template in templateCards:
         w, h = template.shape[::-1]
@@ -27,9 +28,9 @@ while(1):
         threshold = 0.95
         loc = np.where( res >= threshold)
         for pt in zip(*loc[::-1]):
-            cv.rectangle(img_gray, pt, (pt[0] + w, pt[1] + h), (0,255,0), 2)
+            cv.rectangle(img_show, pt, (pt[0] + w, pt[1] + h), (0,255,0), 2)
 
-    cv.imshow("test", img_np)
+    cv.imshow("test", img_show)
     k=cv.waitKey(100) & 0xFF 
 
     if k == ord('q'):
